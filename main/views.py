6d018f5dfcf89dd22fe3dcb751cacbfdf89c99bc
Justin Zapanta <52184197+justinzapanta@users.noreply.github.com>
2026-09-04 18:19:08 +0800

    Add nfcProfile view to render NFC template

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/views/index.html')
+
+def nfcProfile(request):
+    return render(request, 'main/views/nfc.html')
